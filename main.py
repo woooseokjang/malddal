d66f7ff4095e802e5813b0422e0de4e8f4e497e4
Wooseok Jang <mmyu2090@gmail.com>
2021-04-02 17:59:43 +0900

    first commit of v0.8 to github

---
+++ main.py
@@ -0,0 +1,144 @@
+import malddal
+import tkinter as tk
+from threading import Thread, Semaphore
+
+global version
+version = "v0.8"
+
+global directory
+directory = r'%systemdrive%\user\%username%\desktop'
+
+if __name__ == '__main__':
+    flag = False
+    flagSem = Semaphore(1)
+    mymalddal = malddal.malddal()
+    gamepub, resize = mymalddal.getGameFrom()
+    windowflag = False
+    print(gamepub)
+
+    if gamepub == 0:
+        hwnd = mymalddal.getHwndOfDMM()
+    else:
+        hwnd = 0
+
+    if resize:
+        mymalddal.request_admin_and_resize(hwnd)
+
+    charScriptSpec, charScript, charSpec, charIter, charIter2 = mymalddal.read_script()
+
+    lastPrinted = 99999
+
+    window = tk.Tk()
+    window.title("MALDDAL - DCInside 우마무스메 갤러리 " + version)
+    window.minsize(500, 500)
+    def EXIT():
+        flagSem.acquire()
+        global flag
+        flag = True
+        flagSem.release()
+        ocrThread.join()
+        window.destroy()
+        exit(0)
+    window.protocol('WM_DELETE_WINDOW', EXIT)
+
+    def EXITCheck():
+        flagSem.acquire()
+        global flag
+        if flag:
+            print("PROGRAM KILL DETECTED")
+            exit(0)
+        flagSem.release()
+
+    scriptText = []
+    specText = []
+    message = tk.StringVar()
+    for i in range(5):
+        text = tk.StringVar()
+        text2 = tk.StringVar()
+        text.set("Loading")
+        text2.set("Loading")
+        scriptText.append(text)
+        specText.append(text2)
+
+    script0 = tk.Label(window, textvariable=scriptText[0], height=5, width=35, relief="groove")
+    script1 = tk.Label(window, textvariable=scriptText[1], height=5, width=35, relief="groove")
+    script2 = tk.Label(window, textvariable=scriptText[2], height=5, width=35, relief="groove")
+    script3 = tk.Label(window, textvariable=scriptText[3], height=5, width=35, relief="groove")
+    script4 = tk.Label(window, textvariable=scriptText[4], height=5, width=35, relief="groove")
+    script5 = tk.Label(window)
+    spec0 = tk.Label(window, textvariable=specText[0], height=5, width=50, relief="groove")
+    spec1 = tk.Label(window, textvariable=specText[1], height=5, width=50, relief="groove")
+    spec2 = tk.Label(window, textvariable=specText[2], height=5, width=50, relief="groove")
+    spec3 = tk.Label(window, textvariable=specText[3], height=5, width=50, relief="groove")
+    spec4 = tk.Label(window, textvariable=specText[4], height=5, width=50, relief="groove")
+    spec5 = tk.Label(window, textvariable=message)
+
+    def directory_button_click(dir):
+        global directory
+        directory = mymalddal.get_directory(dir)
+
+
+    button0 = tk.Button(window, text="이미지 저장경로 변경",
+                        command=lambda: directory_button_click(directory), width=20, height=3)
+
+    def imagecapture(directory):
+        rgb, temp = mymalddal.getWindowsImage(hwnd)
+        bgr = rgb[..., ::-1].copy()
+        if mymalddal.game_capture(bgr, directory):
+            message.set("저장 실패")
+            window.after(2000, lambda: message.set(""))
+        else:
+            message.set(directory + " <- 저장 완료")
+            window.after(2000, lambda: message.set(""))
+
+    button1 = tk.Button(window, text="게임 캡쳐",
+                        command=lambda: imagecapture(directory), width=20, height=3)
+
+    script0.grid(row=0, column=0)
+    script1.grid(row=1, column=0)
+    script2.grid(row=2, column=0)
+    script3.grid(row=3, column=0)
+    script4.grid(row=4, column=0)
+    script5.grid(row=5, column=0)
+    button0.grid(row=6, column=0)
+    spec0.grid(row=0, column=1)
+    spec1.grid(row=1, column=1)
+    spec2.grid(row=2, column=1)
+    spec3.grid(row=3, column=1)
+    spec4.grid(row=4, column=1)
+    spec5.grid(row=5, column=1)
+    button1.grid(row=6, column=1)
+
+
+    def mainloop():
+        while True:
+            EXITCheck()
+            global image
+            global cap
+            image, cap = mymalddal.getWindowsImage(hwnd)
+            script, spec, printed = mymalddal.OCR(cap, lastPrinted, charScriptSpec, charScript, charSpec, charIter,
+                                                  charIter2)
+            if lastPrinted == printed:
+                continue
+            scriptIter = 0
+            EXITCheck()
+            for sc in script:
+                scriptText[scriptIter].set(sc)
+                scriptIter = scriptIter + 1
+            for sc in range(scriptIter, 5):
+                scriptText[sc].set("N/A")
+            scriptIter = 0
+            for sp in spec:
+                specText[scriptIter].set(sp)
+                scriptIter = scriptIter + 1
+            for sc in range(scriptIter, 5):
+                specText[sc].set("N/A")
+
+    ocrThread = Thread(target=mainloop)
+    ocrThread.start()
+    window.mainloop()
+
+
+
+
+
---
+++ malddal.py
@@ -0,0 +1,213 @@
+import win32gui
+import numpy as np
+from PIL import ImageGrab, Image
+import cv2
+import ctypes
+import pytesseract
+import tkinter as tk
+from tkinter import messagebox, filedialog
+from screeninfo import get_monitors
+import ctypes, sys
+import pandas as pd
+import xlrd
+import time
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def destroyWindow():
+    exit(0)
+
+
+class malddal:
+
+    def __init__(self) -> None:
+        super().__init__()
+        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+        try:
+            print(pytesseract.get_tesseract_version())
+        except pytesseract.pytesseract.TesseractNotFoundError:
+            messagebox.showinfo(title="tesseract 에러", message="tesseract OCR 프로그램을 감지 할 수 없습니다.")
+            exit(0)
+
+    def getGameFrom(self):
+        window = tk.Tk()
+        window.title("프로그램 옵션")
+        window.geometry('300x200')
+        window.protocol('WM_DELETE_WINDOW', destroyWindow)
+        radio_value = tk.IntVar()
+        check_value = tk.BooleanVar()
+
+        radio_button1 = tk.Radiobutton(window, text='  DMM  ', variable=radio_value, value=0)
+        # radio_button2 = tk.Radiobutton(window, text='bluestack', variable=self.radio_value, value=1)
+        radio_button1.pack(pady=20)
+        # radio_button2.pack()
+
+        checkbox = tk.Checkbutton(window, text='최적 해상도 변경', variable=check_value)
+        checkbox.pack(pady=10)
+
+        def clickOK():
+            window.destroy()
+
+        button = tk.Button(window, text="확인", command=clickOK, width=15, height=3)
+
+        button.pack(pady=5)
+
+        window.mainloop()
+        return radio_value.get(), check_value.get()
+
+    def getHwndOfDMM(self):
+        return win32gui.FindWindow(None, "umamusume")
+
+    def getHwndOfBluestack(self):
+        return win32gui.FindWindow(None, "")
+
+    def getWindowsImage(self, hwnd):
+        rgb = np.array(ImageGrab.grab(bbox=win32gui.GetWindowRect(hwnd), all_screens=True))
+        rgb = np.delete(rgb, range(9 + 22), axis=0).copy()
+        rgb = np.delete(rgb, range(9), axis=1).copy()
+        rgb = np.delete(rgb, range(rgb.shape[0] - 9, rgb.shape[0]), axis=0).copy()
+        rgb = np.delete(rgb, range(rgb.shape[1] - 9, rgb.shape[1]), axis=1).copy()
+        bgr = rgb[..., ::-1].copy()
+
+        cap = np.delete(bgr, range(int(bgr.shape[0] / 3)), axis=0).copy()
+        cap = np.delete(cap, range(cap.shape[0] - 2, int(cap.shape[0]/2), -1), axis=0).copy()
+        return bgr, cap
+
+    def resizeWindow(self, hwnd):
+        x0, y0, x1, y1 = win32gui.GetWindowRect(hwnd)
+        monitor = get_monitors()
+        target_width = 540
+        target_height = 960
+        win32gui.MoveWindow(hwnd, x0, y0, target_width, target_height, True)
+
+    def leven(self, aText, bText):
+        aLen = len(aText) + 1
+        bLen = len(bText) + 1
+        array = [[] for a in range(aLen)]
+        for i in range(aLen):
+            array[i] = [0 for a in range(bLen)]
+        for i in range(bLen):
+            array[0][i] = i
+        for i in range(aLen):
+            array[i][0] = i
+        cost = 0
+        for i in range(1, aLen):
+            for j in range(1, bLen):
+                if aText[i - 1] != bText[j - 1]:
+                    cost = 1
+                else:
+                    cost = 0
+                addNum = array[i - 1][j] + 1
+                minusNum = array[i][j - 1] + 1
+                modiNum = array[i - 1][j - 1] + cost
+                minNum = min([addNum, minusNum, modiNum])
+                array[i][j] = minNum
+        return array[aLen - 1][bLen - 1]
+
+    def is_admin(self):
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
+
+    def OCR(self, image, lastPrinted, charScriptSpec, charScript, charSpec, charIter, charIter2):
+        try:
+            ocrOut = pytesseract.image_to_string(image, lang='jpn')
+        except pytesseract.pytesseract.TesseractError:
+            messagebox.showinfo(title="OCR language ERR", message="tesseract 에 일본어팩이 포함되어 있지 않습니다.")
+        ocrScript = ocrOut.split('\n')
+
+        entryScript = ""
+        found = False
+        for script in ocrScript:
+            if len(script) >= 2:
+                # 자주 잡히는 OCR Error 수정 하드코딩
+                script = script.replace("/", "！")
+                script = script.replace("7", "！")
+                print(script)
+                revenList = []
+                for charSc in charScript:
+                    forAppend = self.leven(charSc, script)
+                    revenList.append(forAppend)
+                index = revenList.index(min(revenList))
+                if min(revenList) < 6:
+                    if len(charScript[index]) == min(revenList):
+                        continue
+                    found = True
+                    break;
+        ScriptforPrint = []
+        SpecforPrint = []
+        if found:
+            charIt = charIter[index]
+            startIndex = charIter.index(charIt)
+            nowPrinted = startIndex
+            if startIndex != lastPrinted:
+                ScriptforPrint.append(charScript[startIndex])
+                SpecforPrint.append(charSpec[startIndex].replace("&", "\n"))
+                startIndex = startIndex + 1
+                while charIter2[startIndex] != 1:
+                    ScriptforPrint.append(charScript[startIndex])
+                    SpecforPrint.append(charSpec[startIndex].replace("&", "\n"))
+                    startIndex = startIndex + 1
+        else:
+            nowPrinted = lastPrinted
+        return ScriptforPrint, SpecforPrint, nowPrinted
+
+
+    def request_admin_and_resize(self, hwnd):
+        if self.is_admin():
+            self.resizeWindow(hwnd)
+        else:
+            def destroyWindow():
+                exit(0)
+
+            def clickNoGo():
+                window.destroy()
+
+            def clickGo():
+                # 관리자 권한으로 프로그램을 새로 실행하고 현제 프로그램 종료
+                window.destroy()
+                ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+                exit(0)
+
+            window = tk.Tk()
+            window.title("관리자 권한 필요")
+            window.geometry('350x250')
+            window.protocol('WM_DELETE_WINDOW', destroyWindow)
+            label = tk.Label(window, text="최적 크기로 변경하려면 관리자권한으로 실행해야합니다.")
+            label.pack(pady='20')
+            button2 = tk.Button(window, text="크기 변경 하지 않음", command=clickNoGo, width=15, height=3)
+            button = tk.Button(window, text="권한 요청 및 재실행", command=clickGo, width=15, height=3)
+            button2.pack(pady='20')
+            button.pack(pady='20')
+            window.mainloop()
+
+    def read_script(self):
+        df = pd.read_excel('./character_script_spec.xls')
+        charScriptSpec = df.values.tolist()
+        charScript = [script[1] for script in charScriptSpec]
+        charSpec = [spec[2] for spec in charScriptSpec]
+        charIter = [it[3] for it in charScriptSpec]
+        charIter2 = [it[4] for it in charScriptSpec]
+
+        return charScriptSpec, charScript, charSpec, charIter, charIter2
+
+
+    def get_directory(self, directory):
+        window = tk.Tk()
+        window.withdraw()
+        path = filedialog.askdirectory(parent=window, initialdir=directory, title="이미지 저장 경로 설정")
+        return path
+
+    def game_capture(self, image, path):
+        try:
+            forSave = Image.fromarray(image)
+            fulldir = path + "/" + time.strftime("%Y%m%d-%H%M%S") + ".jpeg"
+            fulldir = fulldir.replace("/", '\\')
+            forSave.save(str(fulldir))
+            return 0
+        except OSError:
+            return 1
