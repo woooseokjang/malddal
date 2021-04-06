05d1e731bb807bba20c846e54e1ec60207e7adda
mmyu2090 <mmyu2090@gmail.com>
2021-04-07 00:45:43 +0900

    skill_crawler updated for 'papago' translation

--- gamewith_skill_crawler.py
+++ gamewith_skill_crawler.py
@@ -5,6 +5,15 @@ import numpy as np
 import time
 import openpyxl
 import xlwt
+import os
+import sys
+import urllib.request
+import json
+import time
+
+with open("./APISETTING.json", 'r') as f:
+    json_data = json.load(f)
+
 
 option = EdgeOptions()
 option.use_chromium = True
@@ -27,10 +36,37 @@ for tr in trs:
         continue
     tds = tr.find_elements_by_tag_name("td")
     for td in tds:
-        skilldata.append(td.text)
+        if td == tds[0]:
+            continue
+        temp = td.text.split("\n")
+        for t in temp:
+            if t == temp[1]:
+                client_id = json_data["client_id"]
+                client_secret = json_data["client_secret"]
+                encText = urllib.parse.quote(t)
+                data = "source=ja&target=ko&text=" + encText
+                url = "https://openapi.naver.com/v1/papago/n2mt"
+                request = urllib.request.Request(url)
+                request.add_header("X-Naver-Client-Id", client_id)
+                request.add_header("X-Naver-Client-Secret", client_secret)
+                response = urllib.request.urlopen(
+                    request, data=data.encode("utf-8"))
+                rescode = response.getcode()
+                if(rescode != 200):
+                    print("PAPAGO API ERR? -> " + rescode)
+                elif(rescode == 200):
+                    response_body = response.read()
+                    resmess = json.loads(response_body.decode('utf-8'))
+                    skilldata.append(
+                        resmess['message']['result']['translatedText'])
+                    print(resmess['message']['result']['translatedText'])
+            else:
+                print(t)
+                skilldata.append(t)
+            time.sleep(0.11)
 
 npData = np.array(skilldata)
-npDataReshaped = npData.reshape(int(len(skilldata) / 2), 4)
+npDataReshaped = npData.reshape(int(len(skilldata) / 2), 2)
 
 df = pd.DataFrame(npDataReshaped, columns=["skillName", "skillInfo"])
 df.to_excel("./skill_spec.xls")
